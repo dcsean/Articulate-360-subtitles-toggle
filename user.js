5297fa36a7c413aedc2a572b22c95b8b11d321c2
whernandez <wolher@gmail.com>
2017-09-22 11:46:14 -0400

    Update user.js

--- user.js
+++ user.js
@@ -6,15 +6,16 @@ var	controlNode = document.getElementsByClassName("controls-group control-bar cs
 	emailsubject = '1 on 1 conversations',
 	emailbody = 'What was the slide number of the issue?%0AWhat was the issue?',
 	mailto = 'onlinelearning@gadventures.com',
-	initInterval = setInterval(init, 1000); // 
+	initInterval = setInterval(init, 1000);
 
+// this function runs one second after file is uploaded
 function init(){
-	//giving IDs to email elements
+	//links objects with DOM IDs
 	emailButtonNode.id = 'emailIcon';
 	subtitlesButtonNode.id = 'subtitlesIcon';
 	slideNumberNode.id = 'slideNumber';
 	progressBarObject.id = 'courseProgressBar'
-	//adding properties to the A tag
+	//adding properties to the href tag
 	emailButtonNode.href = 'mailto:'+mailto+'?subject='+emailsubject+'&body='+emailbody;
 	//if this element on the player exist initialize controlNode variable
 	if (document.getElementsByClassName("controls-group control-bar cs-seekcontrol progress-control")[0]){
@@ -32,15 +33,13 @@ function init(){
 	checkCurrentStatusInterval = setInterval(checkCurrentStatus, 10);
 }
 
-
+// this function runs a 1/10 of a second checking for subtitles visibility
 function checkCurrentStatus(){
-	
 	if(toggle !== null){
 		arrFromList = Array.prototype.slice.call(document.getElementsByClassName("slide-object slide-object-objgroup shown"));
 		subTitlesObjectNode = retrieveSubtitles(arrFromList);
 	}
 	if (toggle == false) {
-
 		if (subTitlesObjectNode.style !== undefined) subTitlesObjectNode.style.display = 'block'
 	}else{
 		if (subTitlesObjectNode.style !== undefined) subTitlesObjectNode.style.display = 'none';
